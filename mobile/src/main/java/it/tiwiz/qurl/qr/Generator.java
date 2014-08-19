2b875b1f932feedd4d8b4a994f0c648b525e3263
Roberto Orgiu <roberto.orgiu@gmail.com>
2014-08-19 23:39:14 +0200

    Adds comment for Generator file, preparing for Gist

--- mobile/src/main/java/it/tiwiz/qurl/qr/Generator.java
+++ mobile/src/main/java/it/tiwiz/qurl/qr/Generator.java
@@ -7,7 +7,18 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-
+/**
+ * This class contains a single method that will generate a QR Code
+ * with given text, foreground and background color, and dimension of the side
+ * of the image.
+ *
+ * This code relies on the ZXing library, which must be added to your project
+ * in order to have the QR Code generated.
+ *
+ * For Gradle/Android environment is enough to declare
+ *      compile 'com.google.zxing:core:3.0.1'
+ * in the <i>dependencies</i> section of the build.gradle file
+ */
 public class Generator {
     public static  Bitmap generateQrCode(String url, int foregroundColor, int backgroundColor, int qrCodeDimension) {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
